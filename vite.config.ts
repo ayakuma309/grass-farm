6a22b6f3f066e5ea0280d60cbf5a2dd0bfe1e222
ayakuma309 <ayako.30901@gmail.com>
2025-11-07 22:10:14 +0900

    add: GitHubの草をピンクに変更

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,13 @@ export default defineConfig({
     rollupOptions: {
       input: {
         popup: resolve(__dirname, 'popup.html'),
+        content: resolve(__dirname, 'src/content.ts'), // 👈追加
       },
       output: {
         entryFileNames: `[name].js`,
         chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`
-      }
+        assetFileNames: `[name].[ext]`,
+      },
     },
     outDir: 'dist',
   },
